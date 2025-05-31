6e5d0309a4083cca154a931bf6f88603023d1caa
Bereketab Haileeyesus Shanka <bereketab.shanka@innowise.com>
2025-06-01 01:34:20 +0200

    Add Spring Data JPA repositories, refactor services to use BookRepository, AuthorRepository, GenreRepository

---
+++ src/main/java/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package library.repository;
+
+import library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthorId(Long authorId);
+}
