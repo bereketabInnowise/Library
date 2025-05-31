b522715e37aa78e05f23c22b5ee1748444bb618f
Bereketab Haileeyesus Shanka <bereketab.shanka@innowise.com>
2025-05-31 23:59:17 +0200

    Task 6 Step 2: Create and export Postman collection for httpbin.org requests

---
+++ src/main/java/library/config/WebConfig.java
@@ -0,0 +1,20 @@
+package library.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class WebConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
