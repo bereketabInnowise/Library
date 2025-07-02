b12a6945ad2e171d58b977c769ee5cf4df10f4e3
Bereketab Haileeyesus Shanka <bereketab.shanka@innowise.com>
2025-07-02 09:17:04 +0200

    Add Spring Security dependencies, create users/roles tables, implement UserDetailsService

---
+++ src/main/java/library/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package library.repository;
+
+import library.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
