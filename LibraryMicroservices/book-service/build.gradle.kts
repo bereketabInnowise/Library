4d52560d28ecee6f6887f006ca07b308ab677157
Bereketab Haileeyesus Shanka <bereketab.shanka@innowise.com>
2025-08-17 23:47:58 +0200

    Deployed and monitored the app in Kubernetes, using Prometheus and Grafana for real-time JVM metrics visualization

--- LibraryMicroservices/book-service/build.gradle.kts
+++ LibraryMicroservices/book-service/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(libs.liquibase)
     implementation(libs.resilience4j.spring.boot3)
     implementation(libs.jjwt.api)
+    implementation(libs.micrometer)
+    implementation(libs.micrometer.core)
     runtimeOnly(libs.jjwt.impl)
     runtimeOnly(libs.jjwt.jackson)
 
